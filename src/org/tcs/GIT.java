0e43f8460eb1529f5c77fcd4e1de20478c1687dc
SriDilip <srikutty1989@gmail.com>
2019-06-06 19:25:17 +0530

    Sample Code Added

---
+++ src/org/tcs/GIT.java
@@ -0,0 +1,33 @@
+package org.tcs;
+
+public class GIT {
+
+
+
+		
+		public void add(int a) {
+			System.out.println(a);
+
+		}
+		
+		public void add(int a,int b) {
+			System.out.println(a+b);
+		}
+		
+		public void add(int a,int b,int c) {
+			System.out.println(a+b+c);
+		}
+		
+		public static void main(String[] args) {
+			GIT s = new GIT();
+			s.add(5);
+			s.add(5, 10);
+			s.add(10,10,10);
+			
+			
+		}
+
+	
+
+
+}
